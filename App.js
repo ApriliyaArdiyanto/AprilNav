5ef4dd605a22c20ff2c9de2370aa0d9576d6d6db
ApriliyaArdiyanto <41560273+ApriliyaArdiyanto@users.noreply.github.com>
2018-10-11 07:24:17 +0700

    Navigation  Login

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image, KeyboardAvoidingView} from 'react-native';
+import LoginScreen from './Screen/LoginScreen'
+import WelcomeScreen from './Screen/WelcomeScreen'
+import SignupScreen from './Screen/SignupScreen'
+import BiodataScreen from './Screen/BiodataScreen'
+import {StackNavigator} from 'react-navigation'
+
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppStackNavigator/>
+        
+    );
+  }
+} 
+
+const AppStackNavigator = new StackNavigator({
+ WelcomeScreen :{screen: WelcomeScreen},
+  LoginScreen:{screen: LoginScreen},
+  SignupScreen:{screen: SignupScreen},
+  BiodataScreen:{screen: BiodataScreen},
+})
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+ 
+  },
+ 
+});
